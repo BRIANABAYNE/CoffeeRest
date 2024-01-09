720c92eeff7df68be3da51c1ebbf2d2b2d340db5
Briana Bayne <brianabayne1@gmail.com>
2024-01-09 13:07:17 -0700

    mlmodel

---
+++ CoffeeRest/CoffeeRest/CoffeeRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeRestApp.swift
+//  CoffeeRest
+//
+//  Created by Briana Bayne on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeRest/CoffeeRest/ContentView.swift
@@ -0,0 +1,77 @@
+//
+//  ContentView.swift
+//  CoffeeRest
+//
+//  Created by Briana Bayne on 1/7/24.
+//
+
+
+// framework
+import CoreML
+import SwiftUI
+
+// ContentView conforms to View - basic protocol
+struct ContentView: View {
+    @State private var wakeup = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeup, displayedComponents: 
+                        .hourAndMinute)
+                .labelsHidden()
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount, in: 1...20)
+            }
+            // title
+            .navigationTitle("Better Rest")
+            .toolbar {
+                Button("Calculate", action: calculateBedtime)
+            }
+        }
+    }
+    
+    func calculateBedtime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeup)
+            // ?? 0 Nil Coal if it can't be read
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount,coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeup - prediction
+            
+            alertTitle = "Your ideal bedtime is... "
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        
+        showingAlert = true
+    }
+}
+
+#Preview {
+    ContentView()
+}
